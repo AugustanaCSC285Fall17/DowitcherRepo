73770d311d0d940e7e8224f95ec3043fac065700
Forrest Stonedahl <fsondahl@gmail.com>
2017-09-14 09:25:48 -0500

    Add libgdx blank/skeleton project

---
+++ adventure-game/desktop/src/edu/augustana/csc285/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package edu.augustana.csc285.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import edu.augustana.csc285.game.AdventureGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AdventureGame(), config);
+	}
+}
