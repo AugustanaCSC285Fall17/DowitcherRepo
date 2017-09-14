06fc1fcc2f9869b842111e1ea45d9cd52bbda1f6
Forrest Stonedahl <forreststonedahl@augustana.edu>
2017-09-14 17:02:25 -0500

    Updated to Java 8 and tweaked game screen size

--- adventure-game/desktop/src/edu/augustana/csc285/game/desktop/DesktopLauncher.java
+++ adventure-game/desktop/src/edu/augustana/csc285/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import edu.augustana.csc285.game.AdventureGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Adventure Game";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new AdventureGame(), config);
 	}
 }
