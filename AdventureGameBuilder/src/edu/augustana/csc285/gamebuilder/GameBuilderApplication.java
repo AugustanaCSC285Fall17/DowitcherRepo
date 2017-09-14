659b29d108b58aeef3ed5f94afbf7e60aee49d9c
Forrest Stonedahl <forreststonedahl@augustana.edu>
2017-09-14 17:05:28 -0500

    Added Eclipse Project for basic JavaFX GameBuilder UI

---
+++ AdventureGameBuilder/src/edu/augustana/csc285/gamebuilder/GameBuilderApplication.java
@@ -0,0 +1,27 @@
+package edu.augustana.csc285.gamebuilder;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GameBuilderApplication extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("MainPane.fxml"));
+	    
+        Scene scene = new Scene(root);
+    
+        primaryStage.setTitle("<YOUR TEAM>'s Game Builder");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args); 
+	}
+}
---
+++ AdventureGameBuilder/src/edu/augustana/csc285/gamebuilder/MainPaneController.java
@@ -0,0 +1,37 @@
+package edu.augustana.csc285.gamebuilder;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class MainPaneController {
+
+	@FXML private TextField uselessTextField;
+	@FXML private Button uselessButton;
+	
+	// Note: Special JavaFX initialize method, called after the Pane has been created.
+	@FXML
+	private void initialize() {
+	}
+	
+	@FXML
+	private void handleUselessButton() {
+		Alert alert = new Alert(AlertType.INFORMATION,"This button is useless!");
+		alert.showAndWait();
+	}
+
+	@FXML
+	private void handleMenuFileClose() {
+		// TODO: eventually offer option to save before closing? 
+		Platform.exit();
+	}
+
+	@FXML
+	private void handleMenuHelpAbout() {
+		new Alert(AlertType.INFORMATION,"Placeholder for about screen").showAndWait();
+	}
+
+}
