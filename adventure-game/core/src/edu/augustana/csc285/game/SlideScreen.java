2c45db3cb49e0d11156c11ad6efba886164b5604
Lars Harvey <larsenharvey16@augustana.edu>
2017-09-26 17:23:20 -0500

    Added functionality: Ability for user to input the option they choose, and space bar to advance to the next slide

--- adventure-game/core/src/edu/augustana/csc285/game/SlideScreen.java
+++ adventure-game/core/src/edu/augustana/csc285/game/SlideScreen.java
@@ -6,10 +6,14 @@ import java.util.Arrays;
 import java.util.List;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 import edu.augustana.csc285.game.datamodel.Slide;
 
 public class SlideScreen implements Screen {
@@ -20,41 +24,50 @@ public class SlideScreen implements Screen {
 	private Texture image;
 	private List<String> options;
 	private OrthographicCamera camera;
+	private int input;
+	private String continueMessage;
+	private int numOptions;
+
 	
 	public SlideScreen(AdventureGame game) {
-		slide = new Slide("test_image", "Directions:", "test url", "", 0, null);
-		//can test for different image with changing test_image to slide_000
+		slide = new Slide("test_image", "You appear to have stumbled upon a testing slide!", "test url", "", 0, null);
+		//can test for different image with changing test_image to test_image2
 		this.game = game;
 		options = new ArrayList<String>();
-		for (int i = 0; i <= 3; i++) {
+		
+		numOptions = 4; // eventually change to slide.getOptionStringList().size();
+		
+		//temporary instantiation of options (for now)
+		for (int i = 0; i < numOptions; i++) {
 			options.add("Go west");
 		}
+		
 		this.image = new Texture("slideImages/" + slide.getImage() + ".png");
+		continueMessage = "";
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, AdventureGame.GAME_SCREEN_WIDTH, 
 								AdventureGame.GAME_SCREEN_HEIGHT);
 	}
 	
 	@Override
-	public void render (float delta) {
+	public void render (float delta) {	
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		game.batch.begin();
-		//Draws the image
-		game.batch.draw(image, BUFFER, 
-				(AdventureGame.GAME_SCREEN_HEIGHT - image.getHeight()) / 2); // this centers the image height-wise 
-		//Draws the url or image description
-		game.font.draw(game.batch, slide.getUrl(), BUFFER,
-				AdventureGame.GAME_SCREEN_HEIGHT / 2 - image.getHeight() / 2 - 5);
-		//Draws the description or the directions for the current slide
-		game.font.draw(game.batch, slide.getDesc() + "\n\nWhat would you like to do?", image.getWidth() + 75, 350);
-		//Draws the options
-		for (int i = 0; i <= 3; i++) {
-			game.font.draw(game.batch, i + 1 + ". " + options.get(i), image.getWidth() + 75, 250 - BUFFER * i);
+		
+		// tell the camera to update its matrices.
+		camera.update();
+
+		// tell the SpriteBatch to render in the
+		// coordinate system specified by the camera.
+		game.batch.setProjectionMatrix(camera.combined);
+		
+		if (image.getHeight() >= image.getWidth()) {
+			drawScreenForPortraitImage();
+		} else {
+			drawScreenForPortraitImage();
 		}
 		
-		game.font.draw(game.batch, "What would you like to do? __", image.getWidth() + 75, 250 - BUFFER * 3 - 30);		
-		game.batch.end();
+		
 	}
 	
 	@Override
@@ -81,7 +94,68 @@ public class SlideScreen implements Screen {
 	public void dispose () {
 		image.dispose();
 	}
+	
+	public void drawScreenForPortraitImage() {
+		game.batch.begin();
+		//Draws the image
+		game.batch.draw(image, BUFFER, 
+				(AdventureGame.GAME_SCREEN_HEIGHT - image.getHeight()) / 2); // this centers the image height-wise 
+		//Draws the url or image description
+		game.font.draw(game.batch, slide.getUrl(), BUFFER,
+				AdventureGame.GAME_SCREEN_HEIGHT / 2 - image.getHeight() / 2 - 5);
+		//Draws the description or the directions for the current slide
+		game.font.draw(game.batch, slide.getDesc() + "\n\nYou may:", image.getWidth() + 75, 350);
+		//Draws the options and checks user input
+		for (int i = 0; i < numOptions; i++) {
+			game.font.draw(game.batch, i + 1 + ". " + options.get(i), image.getWidth() + 100, 290 - BUFFER * i);
+			
+		}
+		//Draws the prompt and includes user input when user enters a number
+		if (input > 0 && input <= numOptions) {
+			game.font.draw(game.batch, "What is your choice?   " + input + "", image.getWidth() + 75, 290 - BUFFER * numOptions - 30);		
+		} else { //Draw without input
+			game.font.draw(game.batch, "What is your choice?   __", image.getWidth() + 75, 290 - BUFFER * numOptions - 30);		
 
+		}
+		//Draws the continue message after player presses a number
+		game.font.draw(game.batch, continueMessage, 590, 30);
+				
+		checkUserInput();
+
+		 
+		
+		game.batch.end();
+	}
+	
+	public void drawScreenForLandscapeImage() {
+		
+	}
+	
+	//Checks user input
+	public void checkUserInput() {
+		if (Gdx.input.isKeyPressed(Keys.NUM_1)) {
+			input = 1;
+			continueMessage = "Press SPACE BAR to continue";
+		} else if (Gdx.input.isKeyPressed(Keys.NUM_2)) {
+			input = 2;
+			continueMessage = "Press SPACE BAR to continue";
+		} else if (Gdx.input.isKeyPressed(Keys.NUM_3)) {
+			input = 3;
+			continueMessage = "Press SPACE BAR to continue";
+		} else if (Gdx.input.isKeyPressed(Keys.NUM_4)) {
+			input = 4;
+			continueMessage = "Press SPACE BAR to continue";
+		}
+		
+		//Continue to next slide when space is pressed
+		if (Gdx.input.isKeyPressed(Keys.SPACE)) {
+			if (input > 0 && input <= numOptions) {
+				game.setScreen(new SlideScreen(game));
+				dispose();
+		}
+			
+		}
+	}
 }
 
 
