354e7260ec52f424e09b023ae015710f2afa59fe
Ryan Philp <rphilp71@gmail.com>
2017-09-26 17:51:23 -0500

    Faisal and I worked a bit on EndScreen

--- adventure-game/core/src/edu/augustana/csc285/game/EndScreen.java
+++ adventure-game/core/src/edu/augustana/csc285/game/EndScreen.java
@@ -1,5 +1,57 @@
 package edu.augustana.csc285.game;
 
-public class EndScreen {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 
+public class EndScreen implements Screen {
+
+    private AdventureGame game;
+    private OrthographicCamera camera;
+
+    public EndScreen(AdventureGame game) {
+        this.game = game;
+        this.camera = new OrthographicCamera();
+        camera.setToOrtho(false,AdventureGame.GAME_SCREEN_WIDTH,AdventureGame.GAME_SCREEN_HEIGHT);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        game.batch.begin();
+        game.font.draw(game.batch, "GAME OVER", 50,50);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
