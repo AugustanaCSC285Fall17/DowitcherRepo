e80ecaf4e4968f486204476a28dd055d0e2251c0
dattran14 <dattran14@augustana.edu>
2017-09-29 11:57:45 -0500

    Add  effect enum and condition enum

---
+++ adventure-game/core/src/edu/augustana/csc285/game/datamodel/Effect.java
@@ -0,0 +1,25 @@
+package edu.augustana.csc285.game.datamodel;
+
+public enum Effect {
+    PLUS,
+    MINUS,
+    TIMES;
+	// change should always be positive, original should always be non negative
+    int applyEffect(int original, int change) {
+        switch (this) {
+            case PLUS:
+                return original + change;
+            case MINUS:
+            	int temp = original - change;
+            	if (temp<0) {
+            		temp = 0;
+            	}
+                return temp;
+            case TIMES:
+                return original*change;
+            default:
+                throw new AssertionError("Unknown effect " + this);
+        }
+    }
+
+}
