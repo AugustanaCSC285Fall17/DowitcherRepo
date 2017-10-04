58f6888799479018497097d24f0ca2fe3eef269c
Tran <DATTRAN14@augustana.edu>
2017-10-03 22:36:15 -0500

    Add inventory screen but haven't test

--- adventure-game/core/src/edu/augustana/csc285/game/SlideScreen.java
+++ adventure-game/core/src/edu/augustana/csc285/game/SlideScreen.java
@@ -62,8 +62,9 @@ public class SlideScreen implements Screen {
 		// Set up stage and table for buttons
 		stage = new Stage(new ScreenViewport());
 		Table buttonTable = new Table();
-		buttonTable.setPosition((3*AdventureGame.GAME_SCREEN_WIDTH)/16 + WIDTH_BUFFER  , 
-				AdventureGame.GAME_SCREEN_HEIGHT - ((5 * AdventureGame.GAME_SCREEN_HEIGHT) / 8) - (HEIGHT_BUFFER * 4) - AdventureGame.GAME_SCREEN_HEIGHT / 24);
+		buttonTable.setPosition((3 * AdventureGame.GAME_SCREEN_WIDTH) / 16 + WIDTH_BUFFER,
+				AdventureGame.GAME_SCREEN_HEIGHT - ((5 * AdventureGame.GAME_SCREEN_HEIGHT) / 8) - (HEIGHT_BUFFER * 4)
+						- AdventureGame.GAME_SCREEN_HEIGHT / 24);
 
 		// Create and add buttons for ActionChoices
 		for (int i = 0; i < visibleOptions.size(); i++) {
@@ -76,14 +77,16 @@ public class SlideScreen implements Screen {
 					if (option.getNextSlideIndex() != -1) {
 						if (option.isFeasible(game.manager.getPlayer())) {
 							game.manager.applyOption(option);
+							game.setScreen(new SlideScreen(game));
+							dispose();
 						} else {
 							System.out.println(option.getRejectMessage());
 							// To do note: handle exception when option is not
 							// feasible
 						}
-						game.setScreen(new SlideScreen(game));
 					} else {
 						game.setScreen(new EndScreen(game));
+						dispose();
 					}
 				}
 
@@ -93,17 +96,18 @@ public class SlideScreen implements Screen {
 			});
 			// for now not measuring the width of the option string but assume
 			// they are the same
-			buttonTable.add(button).width((3*AdventureGame.GAME_SCREEN_WIDTH) /8).height(AdventureGame.GAME_SCREEN_HEIGHT / 24).padTop(HEIGHT_BUFFER).row();
+			buttonTable.add(button).width((3 * AdventureGame.GAME_SCREEN_WIDTH) / 8)
+					.height(AdventureGame.GAME_SCREEN_HEIGHT / 24).padTop(HEIGHT_BUFFER).row();
 		}
 
 		stage.addActor(buttonTable);
-		Label testLabel = new Label(
-				"This is a very very long string This is a very very long string This is a very very long string This is a very very long string This is a very very long string This is a very very long string This is a very very long string This is a very very long string This is a very very long string This is a very very long string This is a very very long string This is a very very long string This is a very very long string This is a very very long string This is a very very long string This is a very very long string This is a very very long string This is a very very long string This is a very very long string This is a very very long string This is a very very long string This is a very very long string This is a very very long string This is a very very long string",
-				DEFAULT_SKIN);
+
+		Label testLabel = new Label(game.manager.getCurrentSlide().getDesc(), DEFAULT_SKIN);
 		testLabel.setWrap(true);
 		ScrollPane scroll = new ScrollPane(testLabel, DEFAULT_SKIN);
-		scroll.setPosition((WIDTH_BUFFER * 2) + (23 * AdventureGame.GAME_SCREEN_HEIGHT) / 32, 
-						AdventureGame.GAME_SCREEN_HEIGHT - (HEIGHT_BUFFER * 2) - (AdventureGame.GAME_SCREEN_HEIGHT / 10) - ((3 * AdventureGame.GAME_SCREEN_HEIGHT) / 8));
+		scroll.setPosition((WIDTH_BUFFER * 2) + (23 * AdventureGame.GAME_SCREEN_HEIGHT) / 32,
+				AdventureGame.GAME_SCREEN_HEIGHT - (HEIGHT_BUFFER * 2) - (AdventureGame.GAME_SCREEN_HEIGHT / 10)
+						- ((3 * AdventureGame.GAME_SCREEN_HEIGHT) / 8));
 		scroll.setSize(AdventureGame.GAME_SCREEN_WIDTH / 2, AdventureGame.GAME_SCREEN_HEIGHT / 3);
 		scroll.setScrollingDisabled(true, false);
 		stage.addActor(scroll);
@@ -116,32 +120,14 @@ public class SlideScreen implements Screen {
 		inventoryButton.setSize(105, 35);
 
 		optionsButton.setPosition(
-				AdventureGame.GAME_SCREEN_WIDTH - inventoryButton.getWidth() - optionsButton.getWidth() - (WIDTH_BUFFER * 2),
+				AdventureGame.GAME_SCREEN_WIDTH - inventoryButton.getWidth() - optionsButton.getWidth()
+						- (WIDTH_BUFFER * 2),
 				AdventureGame.GAME_SCREEN_HEIGHT - optionsButton.getHeight() - HEIGHT_BUFFER);
 		inventoryButton.setPosition(AdventureGame.GAME_SCREEN_WIDTH - inventoryButton.getWidth() - WIDTH_BUFFER,
 				AdventureGame.GAME_SCREEN_HEIGHT - inventoryButton.getHeight() - HEIGHT_BUFFER);
 
 		stage.addActor(optionsButton);
 		stage.addActor(inventoryButton);
-
-		// Enable player to press key
-
-		// slide = new Slide("test_image", "You appear to have stumbled upon a
-		// testing slide!", "test url", "", 0);
-		// // can test for different image with changing test_image to
-		// // test_image2
-		// numOptions = 4; // eventually change to
-		// // slide.getOptionStringList().size();
-		//
-		// // temporary instantiation of options (for now)
-		// for (int i = 0; i < numOptions; i++) {
-		// options.add("Go west");
-		// }
-		//
-		// this.image = new Texture("slideImages/" + slide.getImage() + ".png");
-		// continueMessage = "";
-		//
-		// }
 	}
 
 	@Override
@@ -163,14 +149,16 @@ public class SlideScreen implements Screen {
 					if (option.getNextSlideIndex() != -1) {
 						if (option.isFeasible(game.manager.getPlayer())) {
 							game.manager.applyOption(option);
+							game.setScreen(new SlideScreen(game));
+							dispose();
 						} else {
 							System.out.println(option.getRejectMessage());
 							// To do note: handle exception when option is not
 							// feasible
 						}
-						game.setScreen(new SlideScreen(game));
 					} else {
 						game.setScreen(new EndScreen(game));
+						dispose();
 					}
 				}
 			}
@@ -181,7 +169,8 @@ public class SlideScreen implements Screen {
 		// game.batch.draw(region, x, y, originX, originY, width, height,
 		// scaleX, scaleY, rotation);
 		game.batch.draw(testImage, WIDTH_BUFFER,
-				AdventureGame.GAME_SCREEN_HEIGHT - WIDTH_BUFFER - (5 * AdventureGame.GAME_SCREEN_HEIGHT) / 8 * (testImage.getWidth() / testImage.getHeight()),
+				AdventureGame.GAME_SCREEN_HEIGHT - WIDTH_BUFFER
+						- (5 * AdventureGame.GAME_SCREEN_HEIGHT) / 8 * (testImage.getWidth() / testImage.getHeight()),
 				(5 * AdventureGame.GAME_SCREEN_HEIGHT) / 8,
 				(5 * AdventureGame.GAME_SCREEN_HEIGHT) / 8 * (testImage.getWidth() / testImage.getHeight()));
 		// game.batch.draw(region, x, y, originX, originY, width, height,
@@ -226,71 +215,5 @@ public class SlideScreen implements Screen {
 		image.dispose();
 		stage.dispose();
 	}
-	//
-	// public void drawScreenForPortraitImage() {
-	// game.batch.begin();
-	// // Draws the image
-	// game.batch.draw(image, BUFFER, (AdventureGame.GAME_SCREEN_HEIGHT -
-	// image.getHeight()) / 2); // this
-	// // centers
-	// // the
-	// // image
-	// // height-wise
-	// // Draws the url or image description
-	// game.font.draw(game.batch, slide.getUrl(), BUFFER,
-	// AdventureGame.GAME_SCREEN_HEIGHT / 2 - image.getHeight() / 2 - 5);
-	// // Draws the description or the directions for the current slide
-	// game.font.draw(game.batch, slide.getDesc() + "\n\nYou may:",
-	// image.getWidth() + 75, 350);
-	// // Draws the options and checks user input
-	// for (int i = 0; i < numOptions; i++) {
-	// game.font.draw(game.batch, i + 1 + ". " + options.get(i),
-	// image.getWidth() + 100, 290 - BUFFER * i);
-	//
-	// }
-	// // Draws the prompt and includes user input when user enters a number
-	// if (input > 0 && input <= numOptions) {
-	// game.font.draw(game.batch, "What is your choice? " + input + "",
-	// image.getWidth() + 75,
-	// 290 - BUFFER * numOptions - 30);
-	// } else { // Draw without input
-	// game.font.draw(game.batch, "What is your choice? __", image.getWidth() +
-	// 75,
-	// 290 - BUFFER * numOptions - 30);
-	//
-	// }
-	// // Draws the continue message after player presses a number
-	// game.font.draw(game.batch, continueMessage, 590, 30);
-	//
-	// checkUserInput();
-	//
-	// game.batch.end();
-	// }
-	//
-	// public void drawScreenForLandscapeImage() {
-	//
-	// }
-	//
-	// // Checks user input
-	// public void checkUserInput() {
-	//
-	// // Continue to next slide when space is pressed
-	// if (Gdx.input.isKeyPressed(Keys.SPACE)) {
-	// if (input > 0 && input <= numOptions) {
-	// game.setScreen(new SlideScreen(game));
-	// dispose();
-	// }
-	// } else {
-	// continueMessage = "Press SPACE BAR to continue";
-	// if (Gdx.input.isKeyPressed(Keys.NUM_1)) {
-	// input = 1;
-	// } else if (Gdx.input.isKeyPressed(Keys.NUM_2)) {
-	// input = 2;
-	// } else if (Gdx.input.isKeyPressed(Keys.NUM_3)) {
-	// input = 3;
-	// } else if (Gdx.input.isKeyPressed(Keys.NUM_4)) {
-	// input = 4;
-	// }
-	// }
-	// }
+
 }
