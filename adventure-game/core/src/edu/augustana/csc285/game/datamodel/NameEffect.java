2ea387ac641b9af68619fac2443bdad7d50f1298
Daniel Zwiener <17934820+UngratefulFool@users.noreply.github.com>
2017-10-03 22:12:47 -0500

    Added GenderEffect, NameEffect, and Effect/Conditions in GameDataTester

---
+++ adventure-game/core/src/edu/augustana/csc285/game/datamodel/NameEffect.java
@@ -0,0 +1,17 @@
+package edu.augustana.csc285.game.datamodel;
+
+public class NameEffect implements Effect {
+	private String name;
+	
+	public NameEffect(String name) {
+		this.name = name;
+		
+	}
+	
+	@Override
+	public void applyEffect(Player player) {
+		// TODO Auto-generated method stub
+		player.setName(name);
+	}
+
+}
