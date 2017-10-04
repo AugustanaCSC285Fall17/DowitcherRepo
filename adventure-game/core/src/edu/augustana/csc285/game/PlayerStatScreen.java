d2c12b0ab40c303c93a8e612c38fad9ab3e27dda
Nawaz <faisalnawaz16@augustana.edu>
2017-10-03 19:23:00 -0500

    Playerstatscreen updated --

--- adventure-game/core/src/edu/augustana/csc285/game/PlayerStatScreen.java
+++ adventure-game/core/src/edu/augustana/csc285/game/PlayerStatScreen.java
@@ -1,17 +1,27 @@
 package edu.augustana.csc285.game;
 
+import java.awt.Color;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 import edu.augustana.csc285.game.datamodel.Player;
 
 public class PlayerStatScreen implements Screen {
+	public static final Skin DEFAULT_SKIN = new Skin(Gdx.files.internal("skin/flat-earth-ui.json"));
 	public static final int GAME_SCREEN_WIDTH = 800;
-	public static final int GAME_SCREEN_HEIGHT = 480;	
+	public static final int GAME_SCREEN_HEIGHT = 480;
+	private Viewport viewport;
+	private Stage stage;
 	private Player player;
 	private OrthographicCamera camera;
 	private AdventureGame game;
@@ -22,13 +32,26 @@ public class PlayerStatScreen implements Screen {
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, AdventureGame.GAME_SCREEN_WIDTH, 
 								AdventureGame.GAME_SCREEN_HEIGHT);
-		Skin skin = new Skin(Gdx.files.internal("skin/flat-earth-ui.json"));
-		//still to be completed
+		Label.LabelStyle font = new Label.LabelStyle();
+		Table table = new Table();
+		table.top();
+		table.setFillParent(true);
+	    
+		
+		Label playerStatLabel = new Label ("Player Statistics!", font);
+		table.add(playerStatLabel);
+		stage.addActor(table);
+		
+		//table.center();
+		//table.add("Players'")
+		
+		
 	}
 @Override
 public void render (float delta) {
 	Gdx.gl.glClearColor(0, 0, 0, 1);
 	Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	stage.draw();
 }
 
 @Override
