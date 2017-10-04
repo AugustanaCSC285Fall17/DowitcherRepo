e6b00e5efac3d15669e98ce007ecbf639fa65e95
Nawaz <faisalnawaz16@augustana.edu>
2017-10-03 20:28:18 -0500

    SettingsScreen

--- adventure-game/core/src/edu/augustana/csc285/game/EndScreen.java
+++ adventure-game/core/src/edu/augustana/csc285/game/EndScreen.java
@@ -4,17 +4,31 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 public class EndScreen implements Screen {
+	public static final Skin DEFAULT_SKIN  = new Skin(Gdx.files.internal("skin/flat-earth-ui.json"));
 
     private AdventureGame game;
     private OrthographicCamera camera;
+	private Stage stage;
 
     public EndScreen(AdventureGame game) {
+    	
         this.game = game;
         this.camera = new OrthographicCamera();
         camera.setToOrtho(false,AdventureGame.GAME_SCREEN_WIDTH,
                 AdventureGame.GAME_SCREEN_HEIGHT);
+        
+        Label testLabel = new Label("Your Score:....... Final TEXT/Info about the centre", DEFAULT_SKIN);
+		testLabel.setWrap(true);
+		ScrollPane pane = new ScrollPane(testLabel, DEFAULT_SKIN);
+		pane.setPosition(400, 250);
+		pane.setSize(200, 1500);
+		stage.addActor(pane);
     }
 
     @Override
@@ -28,7 +42,7 @@ public class EndScreen implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         game.batch.begin();
-        game.font.draw(game.batch, "GAME OVER", 50,50);
+        game.font.draw(game.batch, "GAME OVER", 400,50);
         game.batch.end();
     }
 
