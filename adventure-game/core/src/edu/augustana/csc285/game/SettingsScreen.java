454ea7080371a297ace7fdbb4495e99f9fa3533f
Lars Harvey <larsenharvey16@augustana.edu>
2017-10-10 18:26:52 -0500

    Added icons for all current items, slidescreen buttons, added background image, minor 'css' tweaks

--- adventure-game/core/src/edu/augustana/csc285/game/SettingsScreen.java
+++ adventure-game/core/src/edu/augustana/csc285/game/SettingsScreen.java
@@ -103,7 +103,7 @@ public class SettingsScreen implements Screen {
 	
 	@Override
 	public void render (float delta) {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		camera.update();
 		stage.act();
