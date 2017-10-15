c734f323cc37b9c6305105ae90a55abcfe31b626
Daniel Zwiener <17934820+UngratefulFool@users.noreply.github.com>
2017-10-15 18:59:52 -0500

    Created new design of mainPane

--- AdventureGameBuilder/src/edu/augustana/csc285/gamebuilder/MainPanev2Controller.java
+++ AdventureGameBuilder/src/edu/augustana/csc285/gamebuilder/MainPanev2Controller.java
@@ -13,7 +13,10 @@ import java.awt.event.MouseEvent;
 import java.io.File;
 import java.util.HashMap;
 
-public class MainPaneController {
+import edu.augustana.csc285.game.datamodel.Slide;
+import edu.augustana.csc285.game.datamodel.Story;
+
+public class MainPanev2Controller {
 
 	@FXML private TextField descriptionField;
 	@FXML private ChoiceBox slideChoiceBox;
@@ -23,64 +26,43 @@ public class MainPaneController {
 	@FXML private Button saveChangesButton;
 	@FXML private Button descriptionButton;
 	//private Story story;
-	//private Slide currentSlide;
+	private Slide currentSlide;
 	
 	// JavaFX initialize method, called after this Pane is created.
 	@FXML
 	private void initialize() {
-		// Story story = new Story();
-		// Slide slide = new Slide();
+		currentSlide = new Slide();
+		
+	}
+	
+	@FXML
+	public void handleSlideId(String text) {
+		currentSlide.setId(text);
+	}
+	
+	@FXML
+	public void handleSlideDescription() {
+		
+	}
+	
+	@FXML
+	public void handleImage() {
+		
 	}
 
 	@FXML
-	private void handleMenuFileClose() {
-		// TODO: eventually offer option to save before closing? 
-		Platform.exit();
+	public void handleMusic() {
+		
 	}
-
-	@FXML
-	private void handleMenuHelpAbout() {
-		new Alert(AlertType.INFORMATION,"Placeholder for screen to be created").showAndWait();
-	}
-
-	@FXML
-	private void handleSlideChoiceBox() {
-
-	}
-
-	@FXML
-	private void handleImageButton() {
-		File imageFile = getFileFromUser();
-		//currentSlide.setImage(imageFile.getPath());
-	}
-
-	@FXML
-	private void handleMusicButton() {
-		File musicFile = getFileFromUser();
-		//currentSlide.setMusic(musicFile.getPath());
-	}
-
-	@FXML
-	private void handleSlideStatusButton() {
-		//new Alert(AlertType.INFORMATION, currentSlide.toString()).showAndWait();
-	}
-
-	@FXML
-	private void handleDescriptionButton() {
-		//currentSlide.setDesc(descriptionField.getText());
-	}
-
+	
+	
+	
 	private File getFileFromUser() {
 		FileChooser fileBrowser = new FileChooser();
 		return fileBrowser.showOpenDialog(null);
 	}
 	
-    @FXML
-    private void handleSaveChangesButton() {
-    	//save the edited screen
-		//Story.slides.get(currentSlide.getID()) = new Slide(currentSlide);
-    	new Alert(AlertType.INFORMATION,"All Changes Saved").showAndWait();
-    }	
+
 	
 	
 	/**
